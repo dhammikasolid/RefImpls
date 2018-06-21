9d394b7627c311ef1038610223bf845da3b98139
Dammika Nayanajith <dhammikasolid@gmail.com>
2018-06-21 18:51:08 +0530

    Add FullStack project

---
+++ FullStack/API/ValueStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API
+{
+    public class ValueStore
+    {
+        List<string> values = new List<string> { "value1", "value2" };
+
+        public IEnumerable<string> Get()
+        {
+            return values; 
+        }
+
+        public string Get(int id)
+        {
+            return values[id];
+        }
+    }
+}
